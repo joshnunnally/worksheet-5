285b499ba3ed23f908f80741b529bf503b4c6332
Josh Nunnally <jnunnall@nebrwesleyan.edu>
2014-10-16 10:07:03 -0400

    finished 8

--- index.js
+++ index.js
@@ -104,6 +104,78 @@ function four_of_a_kind(list) {
   }
   return 0;
 }
+
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+	// if the other array is a falsy value, return
+	if (!array)
+		return false;
+		
+	// compare lengths - can save a lot of time
+	if (this.length != array.length)
+		return false;
+		
+	for (var i = 0, l=this.length; i < l; i++) {
+		// Check if we have nested arrays
+		if (this[i] instanceof Array && array[i] instanceof Array) {
+			// recurse into the nested arrays
+			if (!this[i].equals(array[i]))
+				return false;
+		}
+		else if (this[i] != array[i]) {
+			// Warning - two different object instances will never be equal: {x:20} != {x:20}
+			return false;
+		}
+	}
+	return true;
+}
+//[1,2,3,4,5].equals([1,2,3,4,5])
+//[2,3,4,5,6].equals([1,2,3,4,5])
+
+function large_straight(list) {
+	var result;
+	if (list.equals([1,2,3,4,5])) {
+		(result = 40);
+	} else if (list.equals([2,3,4,5,6])) {
+		(result = 40);
+	} else {
+		(result = 0);
+	}
+	return result;
+}
+large_straight([3,2,3,4,5]);
+//http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript
+// used for comparing list.
+
+function comparator(first, second) {
+  if (first === second) {
+    return 0;
+  } else {
+    if (first < second) {
+      return - 1;
+    } else {
+      return + 1;
+    }
+  }
+}
+function fullhouse(list) {
+  list.sort(comparator);
+  if (list[0] === list[1]) {
+    if (list[2] === list[4]) {
+      return 25;
+    } else if (list[0] === list[2])
+    {
+      if (list[3] === list[4]) {
+        return 25;
+      } else {
+        return 0;
+      }
+    }
+  }
+  return 0;
+}
+function fullhouse()
+
 	
 
 
